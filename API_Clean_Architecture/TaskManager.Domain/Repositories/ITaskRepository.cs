8d77d790f7f6bbc2545d6e1b3866f453afa725c8
teribele <murilo.teribele.carvalho@gmail.com>
2026-06-11 19:14:03 -0300

    feat(domain): add repository interfaces

---
+++ API_Clean_Architecture/TaskManager.Domain/Repositories/ITaskRepository.cs
@@ -0,0 +1,13 @@
+using TaskManager.Domain.Entities;
+
+namespace TaskManager.Domain.Repositories;
+
+public interface ITaskRepository
+{
+    Task<TaskItem?> GetByIdAsync(Guid id);
+    Task<IEnumerable<TaskItem>> GetAllAsync();
+    Task<IEnumerable<TaskItem>> GetByAssignedUserAsync(Guid userId);
+    Task<TaskItem> AddAsync(TaskItem task);
+    Task<TaskItem> UpdateAsync(TaskItem task);
+    Task<bool> DeleteAsync(Guid id);
+}
